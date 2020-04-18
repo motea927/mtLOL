47faf5dc6c823ce720fbf140a001f2b2ecbd72b6
unknown <motea927@yahoo.com.tw>
2020-04-18 15:20:56 +0800

    finished basic UI

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,20 +1,62 @@
 <template>
-  <div id="app">
-    <landing-page></landing-page>
+  <div id="app" class="md-layout md-alignment-center-center">
+    <app-title-bar></app-title-bar>
+    <div class="container">
+      <app-table></app-table>
+      <div class="status">
+        <p class="text--white">status: 請使用系統管理員開啟!</p>
+      </div>
+      
+    </div>
   </div>
 </template>
 
 <script>
-  import LandingPage from '@/components/LandingPage'
-
+  import AppTitleBar from './components/AppTitleBar/AppTitleBar'
+  import AppTable from './components/AppTable/AppTable'
   export default {
     name: 'mt-lol',
     components: {
-      LandingPage
+      AppTitleBar,
+      AppTable
+    },
+    data () {
+      return {
+        record: 0,
+        status: ''
+      }
     }
   }
 </script>
 
-<style>
+<style lang="scss">
   /* CSS */
+  /* 
+   * 1rem = 100px
+   * .1rem = 10px
+  */
+  html {
+    font-size: 625%;
+  }
+  html, body, #app{
+    width: 100%;
+    height: 100%;
+    font-family: "Helvetica", "Arial","LiHei Pro","黑體-繁","微軟正黑體", sans-serif;
+    border-radius: .05rem;
+  }
+  .container {
+    background-color: #2b2b2b;
+    height: calc(100% - .3rem);
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .status {
+    margin: .5rem;
+  }
+  .text--white {
+    font-size: .16rem;
+    color: #FFF;
+  }
 </style>
---
+++ src/renderer/components/AppTable/AppTable.vue
@@ -0,0 +1,91 @@
+<template>
+  <table class="table">
+      <tr class="table__row--blue">
+        <th>ID</th>
+        <th>戰績1</th>
+        <th>戰績2</th>
+        <th>戰績3</th>
+        <th>戰績4</th>
+      </tr>
+      <tr class="table__row">
+        <td>
+          <p>遊戲名稱</p>
+          <p>牌位</p>
+          <p>分數</p>
+        </td>
+        <td>
+          <p><span class="icon--defeat"></span></p>
+          <p>角色名稱</p>
+          <p>12/12/12</p>
+          <p>2019/0306</p>
+        </td>
+        <td>
+          <p><span class="icon--defeat"></span></p>
+          <p>角色名稱</p>
+          <p>12/12/12</p>
+          <p>2019/0306</p>
+        </td>
+        <td>
+          <p><span class="icon--win"></span></p>
+          <p>角色名稱</p>
+          <p>12/12/12</p>
+          <p>2019/0306</p>
+        </td>
+        <td>
+          <p><span class="icon--defeat"></span></p>
+          <p>角色名稱</p>
+          <p>12/12/12</p>
+          <p>2019/0306</p>
+        </td>
+      </tr>
+      
+  </table>
+</template>
+
+<style lang="scss">
+  .table {
+    width: 80%;
+    margin-top: .3rem;
+    font-size: .16rem;
+    & * {
+      color: black;
+    }
+    &__row {
+      width: 100%;
+      background-color: #f6f6f6;
+      text-align: center;
+      &:nth-of-type(odd) {
+        background-color: #e9e9e9;
+      }
+      & *, &--blue * {
+        padding: .06rem .12rem;
+      }
+      &--blue {
+        background-color: #2980b9;
+        color: #FFF;
+      }
+    }
+  }
+  .icon--defeat {
+    padding: 0;
+    display: inline-block;
+    margin: 0 auto;
+    font-size: .3rem;
+    color: red;
+    cursor: pointer;
+    &::before {
+      content: "\2716";
+    }
+  }
+  .icon--win {
+    padding: 0;
+    display: inline-block;
+    margin: 0 auto;
+    font-size: .3rem;
+    color: green;
+    cursor: pointer;
+    &::before {
+      content: "\2714";
+    }
+  }
+</style>
